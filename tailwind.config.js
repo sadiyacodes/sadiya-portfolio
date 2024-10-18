6b35960f5ff6379db2538d258d315e4c51ccb930
sadiyacodes <99393549+sadiyacodes@users.noreply.github.com>
2024-10-18 23:56:04 +0530

    add : project card
    added Project Card and configured tailwind file for colors to be used.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ export default {
       colors: {
         pink: "#FE486D",
         greybg: "#1C1D21",
+        projectcardbg: "rgb(var(--projectcard-bg))",
+        ll_btn_col: "#F4D0C4",
+        accent: "hsl(var(--accent))",
+        "accent-foreground": "hsl(var(--accent-foreground))",
       },
     },
   },
